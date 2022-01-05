29dd8cb0f9b4c6ad29f0f0826e86dfb6f93201f5
Quentin Gérôme <geromequentin@gmail.com>
2022-01-05 17:44:56 +0100

    chore(Tests): Add some tests for Checkbox & Button

---
+++ components/__tests__/Button.test.tsx
@@ -0,0 +1,20 @@
+import Button from "components/Button"
+import { render, screen, fireEvent } from "@testing-library/react"
+
+describe("Button", () => {
+  test("renders", () => {
+    render(<Button>btn</Button>)
+    const btn = screen.getByText("btn")
+    expect(btn).toBeInTheDocument()
+    expect(btn.classList).toContain("bg-indigo-600") // primary variant
+    expect(btn.classList).toContain("px-3") // md size
+  })
+
+  test("onClick", () => {
+    const onClick = jest.fn()
+    render(<Button onClick={onClick}>btn</Button>)
+
+    fireEvent.click(screen.getByText("btn"))
+    expect(onClick).toHaveBeenCalled()
+  })
+})
