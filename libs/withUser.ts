63245e892e05e4957fb18af7b425f660cbdc62e4
Quentin Gérôme <geromequentin@gmail.com>
2022-01-07 18:25:02 +0100

    chore(Auth): Implement basic login/logout

---
+++ libs/withUser.ts
@@ -0,0 +1,65 @@
+import {
+  GetServerSidePropsContext,
+  GetServerSideProps,
+  GetServerSidePropsResult,
+} from "next"
+import { gql } from "@apollo/client"
+import { ParsedUrlQuery } from "querystring"
+
+import { getApolloClient } from "libs/apollo"
+
+type WithUserRequiredOptions<
+  P = any,
+  Q extends ParsedUrlQuery = ParsedUrlQuery
+> = {
+  getServerSideProps?: GetServerSideProps<P, Q>
+  returnTo?: string
+}
+
+export type GetServerSidePropsResultWithUser<P = any> =
+  GetServerSidePropsResult<P & { user?: null }>
+
+export function withUserRequired(options: WithUserRequiredOptions = {}) {
+  const { getServerSideProps, returnTo } = options
+
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResultWithUser> => {
+    const client = getApolloClient({ headers: ctx.req?.headers })
+    const payload = await client.query({
+      query: gql`
+        query MeQuery {
+          me {
+            email
+            id
+          }
+        }
+      `,
+    })
+
+    if (!payload.data.me) {
+      return {
+        props: {},
+        redirect: {
+          destination: `/login?returnTo=${encodeURIComponent(
+            returnTo || ctx.resolvedUrl
+          )}`,
+          permanent: false,
+        },
+      }
+    } else {
+      let ret: any = { props: {} }
+      if (getServerSideProps) {
+        ret = await getServerSideProps(ctx)
+      }
+
+      return {
+        ...ret,
+        props: {
+          ...ret.props,
+          user: payload.data.me,
+        },
+      }
+    }
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from "next"
+import { withUserRequired } from "libs/withUser"
+import { logout } from "libs/auth"
 
 const Home: NextPage = () => {
-  return null
+  return (
+    <div>
+      Hey <button onClick={() => logout()}>log out</button>
+    </div>
+  )
 }
+
+export const getServerSideProps = withUserRequired()
 
 export default Home
