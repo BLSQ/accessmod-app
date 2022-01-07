65c934228e0e9f272721dad6ba0f823466dc78e7
Quentin Gérôme <geromequentin@gmail.com>
2022-01-07 18:21:12 +0100

    chore: Rewrite apollo client to make it share its server-side cache with the client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import type { ReactElement, ReactNode } from "react"
 import type { AppProps } from "next/app"
 import type { NextPage } from "next"
+import Head from "next/head"
 import { ApolloProvider } from "@apollo/client"
-import client from "apollo-client"
+import { useApollo } from "libs/apollo"
 import DefaultLayout from "components/layouts/DefaultLayout"
 
 import "../styles/globals.css"
@@ -16,11 +17,16 @@ type AppPropsWithLayout = AppProps & {
 }
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
+  const apolloClient = useApollo(pageProps)
   const getLayout =
     Component.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>)
 
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider client={apolloClient}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="" />
+      </Head>
       {getLayout(<Component {...pageProps} />)}
     </ApolloProvider>
   )
