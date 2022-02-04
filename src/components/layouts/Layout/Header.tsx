2db6b24af1019c9f855bc6aba9f584eb2cc3aa80
Quentin Gérôme <geromequentin@gmail.com>
2022-02-04 17:45:17 +0100

    AC-26: User can create a project
    Form is very basic. Some validation need to be done and a <select />  as well.

--- src/components/layouts/Layout/Header.tsx
+++ src/components/layouts/Layout/Header.tsx
@@ -8,45 +8,54 @@ import CreateProjectDialog from "features/CreateProjectDialog";
 import { useState } from "react";
 
 const Header = () => {
+  const [showProjectDialog, setProjectDialog] = useState(true);
   return (
-    <div className="bg-who-blue-main pb-48">
-      <nav className="mb-10">
-        <div className="max-w-8xl mx-auto sm:px-6 lg:px-8">
-          <div className="border-b border-white">
-            <div className="flex items-center justify-between h-16 px-4 sm:px-0">
-              <div className="flex items-center gap-5 text-white h-full">
-                <div className="flex-shrink-0">
-                  <Link href="/">
-                    <a className="flex">
-                      <Image
-                        alt="who logo"
-                        src="/images/WHO-logo-watermark-white.png"
-                        layout="fixed"
-                        height="37"
-                        width="121"
-                      />
-                    </a>
-                  </Link>
+    <>
+      <div className="bg-who-blue-main pb-48">
+        <nav className="mb-10">
+          <div className="max-w-8xl mx-auto sm:px-6 lg:px-8">
+            <div className="border-b border-white">
+              <div className="flex items-center justify-between h-16 px-4 sm:px-0">
+                <div className="flex items-center gap-5 text-white h-full">
+                  <div className="flex-shrink-0">
+                    <Link href="/">
+                      <a className="flex">
+                        <Image
+                          alt="who logo"
+                          src="/images/WHO-logo-watermark-white.png"
+                          layout="fixed"
+                          height="37"
+                          width="121"
+                        />
+                      </a>
+                    </Link>
+                  </div>
+                  <div className="text-xl text-bold uppercase ">Access Mod</div>
+                  <Navbar />
                 </div>
-                <div className="text-xl text-bold uppercase ">Access Mod</div>
-                <Navbar />
-              </div>
-              <div className="hidden md:block">
-                <div className="ml-4 flex items-center md:ml-6">
-                  <Button
-                    variant="primary"
-                    className="mr-4"
-                  >
-                    New Project
-                  </Button>
-                  <UserMenu />
+                <div className="hidden md:block">
+                  <div className="ml-4 flex items-center md:ml-6">
+                    <Button
+                      variant="primary"
+                      className="mr-4"
+                      onClick={() => setProjectDialog(true)}
+                    >
+                      New Project
+                    </Button>
+
+                    <UserMenu />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
+      <CreateProjectDialog
+        open={showProjectDialog}
+        onClose={() => setProjectDialog(false)}
+      />
+    </>
   );
 };
 
