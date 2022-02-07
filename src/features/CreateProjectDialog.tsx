e5f139b00cea12169c2bf1ef5c7e4c78fa3f0576
Quentin Gérôme <geromequentin@gmail.com>
2022-02-07 16:20:08 +0100

    AC-26 CreateProject: Add proper country selection

--- src/features/CreateProjectDialog.tsx
+++ src/features/CreateProjectDialog.tsx
@@ -1,11 +1,15 @@
 import Button from "components/Button";
 import { gql } from "@apollo/client";
 import Dialog from "components/Dialog";
-import Input from "components/Input";
+import Input from "components/forms/Input";
+import Field from "components/forms/Field";
+import SelectInput from "components/forms/SelectInput";
 import useForm from "hooks/useForm";
-import { FormEvent } from "react";
+import { FormEvent, MouseEventHandler } from "react";
 import { useCreateProjectMutation } from "libs/graphql";
 import { useRouter } from "next/router";
+import { countries, Country } from "libs/countries";
+import Spinner from "components/Spinner";
 
 type Props = {
   onClose: () => void;
@@ -15,7 +19,7 @@ type Props = {
 type Form = {
   name: string;
   spatialResolution: string;
-  country: string;
+  country: Country;
 };
 
 const MUTATION = gql`
@@ -33,12 +37,19 @@ const CreateProjectDialog = (props: Props) => {
   const { open, onClose } = props;
   const router = useRouter();
   const [createProjectMutation, { loading }] = useCreateProjectMutation();
-  const { formData, isValid, handleInputChange } = useForm<Form>({
-    validate: (values) =>
-      Boolean(
-        values.name?.length > 0 && values.country && values.spatialResolution
-      ),
-  });
+  const { formData, isValid, handleInputChange, setFieldValue } = useForm<Form>(
+    {
+      validate: (values) =>
+        Boolean(
+          values.name?.length > 0 && values.country && values.spatialResolution
+        ),
+    }
+  );
+
+  const onCancel: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+    onClose();
+  };
 
   const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -48,7 +59,7 @@ const CreateProjectDialog = (props: Props) => {
         input: {
           spatialResolution: parseInt(formData.spatialResolution, 10),
           name: formData.name,
-          country: { code: formData.country },
+          country: formData.country,
         },
       },
     });
@@ -68,34 +79,47 @@ const CreateProjectDialog = (props: Props) => {
         <Dialog.Title>Create a new Project</Dialog.Title>
 
         <Dialog.Content className="px-9 py-8 space-y-4">
-          <Input
-            onChange={handleInputChange}
-            required
-            name="name"
-            label="Project Name"
-            type="text"
-          />
-          <Input
-            onChange={handleInputChange}
-            name="country"
-            label="Country"
-            required
-          />
-          <Input
-            type="number"
-            onChange={handleInputChange}
-            name="spatialResolution"
-            label="Spatial Resolution"
-            required
-          />
+          <Field label="Project name" required name="name">
+            <Input
+              onChange={handleInputChange}
+              required
+              name="name"
+              type="text"
+            />
+          </Field>
+          <Field label="Country" required name="country">
+            <SelectInput
+              required
+              options={countries}
+              labelKey="name"
+              valueKey="code"
+              value={formData.country}
+              onChange={(value) => setFieldValue("country", value)}
+            />
+          </Field>
+
+          <Field required label="Spatial Resolution" name="spatialResolution">
+            <Input
+              type="number"
+              onChange={handleInputChange}
+              name="spatialResolution"
+              label="Spatial Resolution"
+              required
+            />
+          </Field>
         </Dialog.Content>
 
         <Dialog.Actions>
-          <Button role="button" onClick={onClose} variant="outlined">
+          <Button role="button" onClick={onCancel} variant="outlined">
             Cancel
           </Button>
-          <Button disabled={!isValid || loading} role="submit">
-            Create
+          <Button
+            disabled={!isValid || loading}
+            role="submit"
+            className="space-x-2"
+          >
+            {loading && <Spinner size="sm" />}
+            <span>Create</span>
           </Button>
         </Dialog.Actions>
       </form>
