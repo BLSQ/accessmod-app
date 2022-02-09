38f98f98c4cf29592e7b1d0eb84ac9a014319ce9
Quentin Gérôme <geromequentin@gmail.com>
2022-02-09 10:40:44 +0100

    Implement a FilesetRolePicker

---
+++ src/features/FilesetRolePicker.tsx
@@ -0,0 +1,67 @@
+import { gql, useQuery } from "@apollo/client";
+import clsx from "clsx";
+import Button from "components/Button";
+import SelectInput, { DefaultComponents } from "components/forms/SelectInput";
+import { getFormatLabel } from "libs/constants";
+import { useFilesetRolePickerQuery } from "libs/graphql";
+
+type Props = {
+  value: { [key: string]: string };
+  onChange: (value: any) => void;
+};
+
+const RoleOption = (props: any) => {
+  return (
+    <DefaultComponents.Option {...props}>
+      <div className="flex">
+        <div className="flex-1 flex flex-col">
+          <span className="text-sm font-bold">{props.data.name}</span>
+          <span className={clsx("text-xs mt-1")}>Description of the role</span>
+        </div>
+        <Button
+          variant="pill"
+          size="sm"
+          className="bg-who-blue-light self-center"
+        >
+          {getFormatLabel(props.data.format)}
+        </Button>
+      </div>
+    </DefaultComponents.Option>
+  );
+};
+
+const COMPONENTS = {
+  Option: RoleOption,
+};
+
+const QUERY = gql`
+  query FilesetRolePicker {
+    accessmodFilesetRoles {
+      items {
+        id
+        name
+        format
+        createdAt
+        updatedAt
+      }
+    }
+  }
+`;
+
+const FilesetRolePicker = (props: Props) => {
+  const { value, onChange } = props;
+  const { data } = useFilesetRolePickerQuery();
+
+  return (
+    <SelectInput
+      options={data?.accessmodFilesetRoles?.items ?? []}
+      value={value}
+      onChange={onChange}
+      labelKey="name"
+      valueKey="id"
+      components={COMPONENTS}
+    />
+  );
+};
+
+export default FilesetRolePicker;
