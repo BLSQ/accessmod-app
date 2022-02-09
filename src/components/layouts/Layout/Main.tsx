3c0af5d49c71ead9a2e8c3004c065da3471c70ee
Quentin Gérôme <geromequentin@gmail.com>
2022-02-09 10:41:07 +0100

    Layout: Center main element in the page

--- src/components/layouts/Layout/Main.tsx
+++ src/components/layouts/Layout/Main.tsx
@@ -6,7 +6,7 @@ const Main = (props: MainProps) => {
   const { children } = props;
   return (
     <main className="-mt-48 pb-8 flex-1 flex flex-col">
-      <div className="max-w-8xl w-full pb-12 px-4 sm:px-6 lg:px-8 flex-1 flex flex-col">
+      <div className="max-w-8xl w-full pb-12 px-4 sm:px-6 lg:px-8 flex-1 flex flex-col mx-auto">
         {children}
       </div>
     </main>
