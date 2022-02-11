29346e78a6edfd869818aa5b1b896bd0fc50e15f
Quentin Gérôme <geromequentin@gmail.com>
2022-02-11 15:40:58 +0100

    Add Time component & Luxon

---
+++ src/components/Time.tsx
@@ -0,0 +1,20 @@
+import { DateTime, DateTimeOptions } from "luxon";
+import { memo } from "react";
+
+type Props = {
+  datetime: string;
+  className?: string;
+  format?: DateTimeOptions;
+};
+
+const Time = (props: Props) => {
+  const datetime = DateTime.fromISO(props.datetime);
+
+  return (
+    <time dateTime={datetime.toISO()} className={props.className}>
+      {datetime.toLocaleString(props.format ?? DateTime.DATETIME_SHORT)}
+    </time>
+  );
+};
+
+export default memo(Time);
