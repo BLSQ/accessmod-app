0fd26aa457d9af4c690dc5a28f2ff3082a757aac
Quentin Gérôme <geromequentin@gmail.com>
2022-02-22 10:17:39 +0100

    ProjectNavbar: Use Aside instead of Block

--- src/features/ProjectNavbar.tsx
+++ src/features/ProjectNavbar.tsx
@@ -1,8 +1,6 @@
 import { gql } from "@apollo/client";
-import clsx from "clsx";
-import Block from "components/Block";
+import Aside from "components/Aside";
 import { ProjectNavbar_ProjectFragment } from "libs/graphql";
-import Link from "next/link";
 
 const ProjectNavbar = (props: {
   project: ProjectNavbar_ProjectFragment;
@@ -10,32 +8,21 @@ const ProjectNavbar = (props: {
 }) => {
   const { project, className } = props;
   return (
-    <Block
-      as="nav"
-      background="bg-midnight-blue"
-      className={clsx(
-        "text-white w-full py-4 md:px-4 xl:px-7 md:py-6 h-72",
-        className
-      )}
-    >
-      <ul className="list-none">
-        <li className="w-full transition-all hover:bg-picton-blue-600 rounded-md">
-          <Link href={`/projects/${encodeURIComponent(project.id)}/`}>
-            <a className="flex px-3 py-2 ">Project Summary</a>
-          </Link>
-        </li>
-        <li className="w-full transition-all hover:bg-picton-blue rounded-md">
-          <Link href={`/projects/${encodeURIComponent(project.id)}/analysis`}>
-            <a className="flex px-3 py-2 ">Analysis</a>
-          </Link>
-        </li>
-        <li className="w-full transition-all hover:bg-picton-blue rounded-md">
-          <Link href={`/projects/${encodeURIComponent(project.id)}/datasets`}>
-            <a className="flex px-3 py-2 ">Datasets</a>
-          </Link>
-        </li>
-      </ul>
-    </Block>
+    <Aside as="nav" className={className}>
+      <Aside.LinkItem href={`/projects/${encodeURIComponent(project.id)}/`}>
+        Project Summary
+      </Aside.LinkItem>
+      <Aside.LinkItem
+        href={`/projects/${encodeURIComponent(project.id)}/analysis`}
+      >
+        Analysis
+      </Aside.LinkItem>
+      <Aside.LinkItem
+        href={`/projects/${encodeURIComponent(project.id)}/datasets`}
+      >
+        Datasets
+      </Aside.LinkItem>
+    </Aside>
   );
 };
 
