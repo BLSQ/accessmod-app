b4609d898faf5afafb982ab1547d5913a1a7cc53
Quentin Gérôme <geromequentin@gmail.com>
2022-02-23 17:32:45 +0100

    Remove unused tests

--- __tests__/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Home from "pages/index";
-
-describe("Home", () => {
-  it("renders the home", () => {
-    render(<Home />);
-  });
-});
