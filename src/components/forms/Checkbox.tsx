ef34442051e36f2de4cef3cd63edd0429773a32c
Quentin Gérôme <geromequentin@gmail.com>
2022-04-11 09:37:50 +0200

    Checkbox: Add indeterminate state

--- src/components/forms/Checkbox.tsx
+++ src/components/forms/Checkbox.tsx
@@ -1,27 +1,44 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, useEffect, useRef } from "react";
 import clsx from "clsx";
 
 interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   description?: string;
+  indeterminate?: boolean;
 }
 
 const Checkbox = (props: CheckboxProps) => {
-  const { id, name, label, description, className, ...delegated } = props;
-
+  const {
+    id,
+    name,
+    label,
+    indeterminate,
+    description,
+    className,
+    ...delegated
+  } = props;
+  const inputRef = useRef<HTMLInputElement>(null);
   const inputClassName = clsx(
     "form-checkbox h-4 w-4 text-lochmara border-gray-300 rounded focus:ring-0 focus:ring-offset-0",
     className
   );
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.indeterminate = Boolean(indeterminate);
+    }
+  }, [indeterminate, inputRef]);
+
   return (
     <div className="relative flex items-start">
-      <div className="flex items-center h-5">
+      <div className="flex h-5 items-center">
         <input
           id={id ?? name}
           name={name}
           type="checkbox"
           className={inputClassName}
           {...delegated}
+          ref={inputRef}
         />
       </div>
       {(label || description) && (
