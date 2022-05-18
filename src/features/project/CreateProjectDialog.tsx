beaac775d94b292ac06de67817732a3be9a90150
Quentin Gérôme <geromequentin@gmail.com>
2022-05-18 13:43:29 +0200

    Project: Disable the creation of a project using a raster temporarily (#70)

--- src/features/project/CreateProjectDialog.tsx
+++ src/features/project/CreateProjectDialog.tsx
@@ -5,24 +5,17 @@ import Dialog from "components/Dialog";
 import Field from "components/forms/Field";
 import Textarea from "components/forms/Textarea";
 import Spinner from "components/Spinner";
-import DatasetFileInput from "features/dataset/DatasetFileInput";
-import FilesetRolePicker from "features/FilesetRolePicker";
 import useCacheKey from "hooks/useCacheKey";
 import useForm from "hooks/useForm";
 import { Country } from "libs/countries";
+import { createDataset } from "libs/dataset";
 import {
-  createDataset,
-  getExtentCoords,
-  getRasterMetadata,
-} from "libs/dataset";
-import {
-  AccessmodFilesetFormat,
   CreateAccessmodProjectError,
   useCreateProjectMutation,
 } from "libs/graphql";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
-import { MouseEventHandler, useEffect, useState } from "react";
+import { MouseEventHandler, useState } from "react";
 import CountryPicker from "../CountryPicker";
 
 type Props = {
@@ -53,14 +46,6 @@ const MUTATION = gql`
 const CreateProjectDialog = (props: Props) => {
   const { open, onClose } = props;
   const router = useRouter();
-  const [uploadProgress, setUploadProgress] = useState(0);
-  const [configurationDataset, setConfigurationDataset] = useState<{
-    role: any;
-    files: File[];
-  }>({
-    role: null,
-    files: [],
-  });
   const [createProjectMutation] = useCreateProjectMutation();
   const { t } = useTranslation();
   const form = useForm<Form>({
@@ -105,21 +90,6 @@ const CreateProjectDialog = (props: Props) => {
       const { success, project, errors } = mutation.data.createAccessmodProject;
 
       if (success && project) {
-        if (configurationDataset.files && configurationDataset.role) {
-          // Create this first dataset as well
-          await createDataset(
-            {
-              name: configurationDataset.files[0].name
-                .split(".")
-                .slice(0, -1)
-                .join(""),
-              project,
-              role: configurationDataset.role,
-              files: configurationDataset.files,
-            },
-            { onProgress: setUploadProgress }
-          );
-        }
         router.push(`/projects/${encodeURIComponent(project.id)}`);
         clearCache();
         onClose();
@@ -138,17 +108,6 @@ const CreateProjectDialog = (props: Props) => {
     onClose();
   };
 
-  useEffect(() => {
-    if (!configurationDataset.role) return;
-    if (configurationDataset.files.length > 0) {
-      getRasterMetadata(configurationDataset.files[0]).then((metadata) => {
-        form.setFieldValue("crs", metadata?.crs);
-        form.setFieldValue("spatialResolution", metadata.resolution);
-        form.setFieldValue("extent", getExtentCoords(metadata.bounds));
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [configurationDataset]);
   return (
     <Dialog open={open} onClose={onClose} maxWidth="max-w-xl">
       <form onSubmit={form.handleSubmit}>
@@ -192,71 +151,34 @@ const CreateProjectDialog = (props: Props) => {
             </Textarea>
           </Field>
 
-          <fieldset className="pt-2">
-            <p className="mb-6 text-base text-gray-600">
-              {t(
-                "You can configure your project by either uploading a raster file or by entering the spatial resolution and coordinate reference system."
-              )}
-            </p>
-            <div className="-m-3 divide-y divide-gray-300 rounded-md bg-gray-50 p-3">
-              <div className="grid grid-cols-2 items-end gap-2 pb-5">
-                <Field
-                  required
-                  label="Spatial Resolution"
-                  value={form.formData.spatialResolution}
-                  help={t(
-                    "The spatial resolution refers to the linear spacing of a measurement."
-                  )}
-                  name="spatialResolution"
-                  type="number"
-                  onChange={form.handleInputChange}
-                  disabled={form.isSubmitting}
-                  error={
-                    form.touched.spatialResolution &&
-                    form.errors.spatialResolution
-                  }
-                />
-                <Field
-                  required
-                  label={t("Coordinate Reference System")}
-                  name="crs"
-                  type="number"
-                  value={form.formData.crs}
-                  help={t(
-                    "A coordinate reference system (CRS) defines, with the help of coordinates, how the two-dimensional, projected map in your GIS is related to real places on the earth."
-                  )}
-                  onChange={form.handleInputChange}
-                  disabled={form.isSubmitting}
-                  error={form.touched.crs && form.errors.crs}
-                />
-              </div>
-              <div className="space-y-4 pt-5">
-                <Field label={t("Role")} name="role">
-                  <FilesetRolePicker
-                    disabled={form.isSubmitting}
-                    format={AccessmodFilesetFormat.Raster}
-                    onChange={(role) =>
-                      setConfigurationDataset({ ...configurationDataset, role })
-                    }
-                    value={configurationDataset.role}
-                  />
-                </Field>
-                <Field label={t("Raster File")} name="raster">
-                  <DatasetFileInput
-                    className="bg-white"
-                    role={configurationDataset?.role}
-                    files={configurationDataset.files}
-                    onChange={(files) =>
-                      setConfigurationDataset({
-                        ...configurationDataset,
-                        files,
-                      })
-                    }
-                  />
-                </Field>
-              </div>
-            </div>
-          </fieldset>
+          <Field
+            required
+            label="Spatial Resolution (in meters)"
+            value={form.formData.spatialResolution}
+            help={t(
+              "The spatial resolution refers to the linear spacing of a measurement."
+            )}
+            name="spatialResolution"
+            type="number"
+            onChange={form.handleInputChange}
+            disabled={form.isSubmitting}
+            error={
+              form.touched.spatialResolution && form.errors.spatialResolution
+            }
+          />
+          <Field
+            required
+            label={t("Coordinate Reference System")}
+            name="crs"
+            type="number"
+            value={form.formData.crs}
+            help={t(
+              "A coordinate reference system (CRS) defines, with the help of coordinates, how the two-dimensional, projected map in your GIS is related to real places on the earth."
+            )}
+            onChange={form.handleInputChange}
+            disabled={form.isSubmitting}
+            error={form.touched.crs && form.errors.crs}
+          />
 
           {form.submitError && (
             <p className={clsx("text-sm", "text-red-600")}>
@@ -279,13 +201,7 @@ const CreateProjectDialog = (props: Props) => {
             className="space-x-2"
           >
             {form.isSubmitting && <Spinner size="xs" />}
-            {form.isSubmitting && uploadProgress > 0 ? (
-              <span>
-                {t("Uploading {{progress}}%", { progress: uploadProgress })}
-              </span>
-            ) : (
-              <span>{t("Create")}</span>
-            )}
+            <span>{t("Create")}</span>
           </Button>
         </Dialog.Actions>
       </form>
