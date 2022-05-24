3be3bef3692ee7247bfa49b8ed4ecaf4ecd6dd44
Quentin Gérôme <geromequentin@gmail.com>
2022-05-24 14:52:40 +0200

    Disable dataset metadata edit (temporarily)

--- src/libs/dataset.ts
+++ src/libs/dataset.ts
@@ -299,8 +299,9 @@ export async function createDataset(
     project,
     role,
     automatic = false,
+    // FIXME: Comment metadata until the backend supports it in for this mutation.
+    // metadata = {},
     files = [],
-    metadata = {},
   }: CreateDatasetInput,
   { onProgress }: CreateDatasetOptions = {}
 ) {
@@ -310,7 +311,7 @@ export async function createDataset(
     projectId: project.id,
     roleId: role.id,
     automatic,
-    metadata,
+    // metadata,
     name:
       name ??
       `Automatic ${role.name} (${DateTime.now().toLocaleString(
