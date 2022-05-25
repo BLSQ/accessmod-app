b04298a52324e838e88731f0506e2eae677ead7e
Quentin Gérôme <geromequentin@gmail.com>
2022-05-25 09:49:27 +0200

    Increase dataset invalid color for better user understanding

--- src/features/dataset/DatasetStatusIcon.tsx
+++ src/features/dataset/DatasetStatusIcon.tsx
@@ -4,7 +4,7 @@ import {
   PauseIcon,
   CloudIcon,
 } from "@heroicons/react/outline";
-import { ExclamationCircleIcon } from "@heroicons/react/solid";
+import { ExclamationIcon } from "@heroicons/react/solid";
 import Tooltip from "components/Tooltip";
 import {
   AccessmodFilesetStatus,
@@ -26,31 +26,31 @@ const DatasetStatusIcon = ({
             message: dataset.metadata?.validation_error ?? t("Unknown error"),
           })}
         >
-          <ExclamationCircleIcon className="h-4 text-amber-300" />
+          <ExclamationIcon className="h-5 text-red-400" />
         </Tooltip>
       );
     case AccessmodFilesetStatus.Valid:
       return (
         <Tooltip label={t("Valid")}>
-          <CheckCircleIcon className="h-4 text-teal-400" />
+          <CheckCircleIcon className="h-5 text-teal-400" />
         </Tooltip>
       );
     case AccessmodFilesetStatus.ToAcquire:
       return (
         <Tooltip label={t("This dataset will be generated automatically.")}>
-          <CloudIcon className="h-4 w-4 text-lochmara-100" />
+          <CloudIcon className="h-5 w-5 text-lochmara-100" />
         </Tooltip>
       );
     case AccessmodFilesetStatus.Pending:
       return (
         <Tooltip label={t("Pending")}>
-          <PauseIcon className="h-4 text-lochmara-100" />
+          <PauseIcon className="h-5 text-lochmara-100" />
         </Tooltip>
       );
     case AccessmodFilesetStatus.Validating:
       return (
         <Tooltip label={t("Validating")}>
-          <PauseIcon className="h-4 text-lochmara-100" />
+          <PauseIcon className="h-5 text-lochmara-100" />
         </Tooltip>
       );
     default:
