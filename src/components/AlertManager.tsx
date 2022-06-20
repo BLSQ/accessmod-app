b22f74dbbfdec14edc9330d0343619d4093ac76f
Quentin Gérôme <geromequentin@gmail.com>
2022-06-20 15:32:52 +0200

    Alert system

---
+++ src/components/AlertManager.tsx
@@ -0,0 +1,26 @@
+import { Portal } from "@headlessui/react";
+import { useListener } from "hooks/useEmitter";
+import { useState } from "react";
+import Alert from "./Alert";
+
+const AlertManager = () => {
+  const [alert, setAlert] = useState<null | {
+    message: string;
+    type?: "error" | "info" | "warning";
+  }>(null);
+
+  useListener("displayErrorAlert", (event) => {
+    setAlert(event.detail);
+  });
+
+  if (!alert) return null;
+  return (
+    <Portal>
+      <Alert icon={alert.type ?? "info"} onClose={() => setAlert(null)}>
+        <p>{alert.message}</p>
+      </Alert>
+    </Portal>
+  );
+};
+
+export default AlertManager;
