8ae33b765017d6dffe092b23e0c957e62bf9a1d3
Quentin Gérôme <geromequentin@gmail.com>
2022-06-29 17:30:11 +0200

    Do not render visualization if missing metadata; ErrorBoundary around map & matadata

--- src/features/analysis/AccessibilityAnalysisParameters.tsx
+++ src/features/analysis/AccessibilityAnalysisParameters.tsx
@@ -5,13 +5,14 @@ import {
   AccessibilityAnalysisParameters_ProjectFragment,
 } from "libs/graphql";
 import { useTranslation } from "next-i18next";
+import Link from "next/link";
 
 type Props = {
   analysis: AccessibilityAnalysisParameters_AnalysisFragment;
   project: AccessibilityAnalysisParameters_ProjectFragment;
 };
 
-const AccessibilityAnalysisParameters = ({ analysis }: Props) => {
+const AccessibilityAnalysisParameters = ({ analysis, project }: Props) => {
   const { t } = useTranslation();
 
   if (analysis.__typename !== "AccessmodAccessibilityAnalysis") {
@@ -20,25 +21,109 @@ const AccessibilityAnalysisParameters = ({ analysis }: Props) => {
   return (
     <DescriptionList>
       <DescriptionList.Item label={t("Land cover")}>
-        {analysis.landCover?.name ?? "-"}
+        {analysis.landCover ? (
+          <Link
+            href={`/projects/${encodeURIComponent(
+              project.id
+            )}/datasets/${encodeURIComponent(analysis.landCover.id)}/`}
+          >
+            <a className="flex items-center text-gray-800 hover:text-gray-600">
+              {analysis.landCover.name}
+            </a>
+          </Link>
+        ) : (
+          "-"
+        )}
       </DescriptionList.Item>
       <DescriptionList.Item label={t("Transport network")}>
-        {analysis.transportNetwork?.name ?? "-"}
+        {analysis.transportNetwork ? (
+          <Link
+            href={`/projects/${encodeURIComponent(
+              project.id
+            )}/datasets/${encodeURIComponent(analysis.transportNetwork.id)}/`}
+          >
+            <a className="flex items-center text-gray-800 hover:text-gray-600">
+              {analysis.transportNetwork.name}
+            </a>
+          </Link>
+        ) : (
+          "-"
+        )}
       </DescriptionList.Item>
       <DescriptionList.Item label={t("Digital elevation model")}>
-        {analysis.dem?.name ?? "-"}
+        {analysis.dem ? (
+          <Link
+            href={`/projects/${encodeURIComponent(
+              project.id
+            )}/datasets/${encodeURIComponent(analysis.dem.id)}/`}
+          >
+            <a className="flex items-center text-gray-800 hover:text-gray-600">
+              {analysis.dem.name}
+            </a>
+          </Link>
+        ) : (
+          "-"
+        )}
       </DescriptionList.Item>
       <DescriptionList.Item label={t("Stack")}>
-        {analysis.stack?.name ?? "-"}
+        {analysis.stack ? (
+          <Link
+            href={`/projects/${encodeURIComponent(
+              project.id
+            )}/datasets/${encodeURIComponent(analysis.stack.id)}/`}
+          >
+            <a className="flex items-center text-gray-800 hover:text-gray-600">
+              {analysis.stack.name}
+            </a>
+          </Link>
+        ) : (
+          "-"
+        )}
       </DescriptionList.Item>
       <DescriptionList.Item label={t("Health facilities")}>
-        {analysis.healthFacilities?.name ?? "-"}
+        {analysis.healthFacilities ? (
+          <Link
+            href={`/projects/${encodeURIComponent(
+              project.id
+            )}/datasets/${encodeURIComponent(analysis.healthFacilities.id)}/`}
+          >
+            <a className="flex items-center text-gray-800 hover:text-gray-600">
+              {analysis.healthFacilities.name}
+            </a>
+          </Link>
+        ) : (
+          "-"
+        )}
       </DescriptionList.Item>
       <DescriptionList.Item label={t("Barriers")}>
-        {analysis.barrier?.name ?? "-"}
+        {analysis.barrier ? (
+          <Link
+            href={`/projects/${encodeURIComponent(
+              project.id
+            )}/datasets/${encodeURIComponent(analysis.barrier.id)}/`}
+          >
+            <a className="flex items-center text-gray-800 hover:text-gray-600">
+              {analysis.barrier.name}
+            </a>
+          </Link>
+        ) : (
+          "-"
+        )}
       </DescriptionList.Item>
       <DescriptionList.Item label={t("Water")}>
-        {analysis.water?.name ?? "-"}
+        {analysis.water ? (
+          <Link
+            href={`/projects/${encodeURIComponent(
+              project.id
+            )}/datasets/${encodeURIComponent(analysis.water.id)}/`}
+          >
+            <a className="flex items-center text-gray-800 hover:text-gray-600">
+              {analysis.water.name}
+            </a>
+          </Link>
+        ) : (
+          "-"
+        )}
       </DescriptionList.Item>
       <DescriptionList.Item label={t("Travel Scenario")}>
         {Object.keys(analysis.movingSpeeds ?? {}) ? (
