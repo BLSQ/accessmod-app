70a131cc333c5c9fd3c51b608fae7bddeb8bfe45
Quentin Gérôme <geromequentin@gmail.com>
2022-07-19 12:23:31 +0200

    Fix upload of files (removed acl header)

--- src/libs/dataset.ts
+++ src/libs/dataset.ts
@@ -423,10 +423,6 @@ export async function createDataset(
 
       return {
         url: data!.uploadUrl as string,
-        headers: {
-          // FIXME: This header should be passed by the backend somehow
-          "x-amz-acl": "private",
-        },
       };
     },
     onAfterFileUpload: async (
