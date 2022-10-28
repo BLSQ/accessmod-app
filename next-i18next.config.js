9146f8c11c371de93fed5262622307145cb4b892
Quentin Gérôme <geromequentin@gmail.com>
2022-10-28 14:46:09 +0200

    fix(i18n): Do not redirect french users to /fr on login

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   i18n: {
+    localeDetection: false, // Disable locale detection for now. It prevents next to redirect user to /<locale> on /
     defaultNS: "messages",
     defaultLocale: "en",
-    fallbackLng: "en",
     locales: ["en", "fr"],
-    pluralSeparator: "___",
-    // This option will reload your translations whenever serverSideTranslations is called
-    reloadOnPrerender: true,
-
-    // allow an empty value to count as invalid (by default is true)
-    returnEmptyString: false,
   },
+  fallbackLng: "en",
+  pluralSeparator: "___",
+  // This option will reload your translations whenever serverSideTranslations is called
+  reloadOnPrerender: true,
+
+  // allow an empty value to count as invalid (by default is true)
+  returnEmptyString: false,
 };
